c3c1b1a2aa2b0f6955545b2cdfcdae6b4429e02d
Hannahid <37037567+Hannahid@users.noreply.github.com>
2021-02-21 13:55:44 +0800

    Update AES.cpp

--- AES.cpp
+++ AES.cpp
@@ -1,208 +1,396 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include<string.h>
-#include <math.h>
-#include<algorithm>
-using namespace std;
-typedef long long ll;
-int e, d, n;
-
-int gcd(int a, int b)  //求最大公约数
+#include <bitset>
+#include <string>
+using namespace std; 
+typedef bitset<8> byte;
+typedef bitset<32> word;
+ 
+const int Nr = 10;  // AES-128需要 10 轮加密
+const int Nk = 4;   // Nk 表示输入密钥的 word 个数
+ 
+byte S_Box[16][16] = {
+	{0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76},
+	{0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0},
+	{0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15},
+	{0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75},
+	{0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84},
+	{0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF},
+	{0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8},
+	{0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2},
+	{0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73},
+	{0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB},
+	{0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79},
+	{0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08},
+	{0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A},
+	{0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E},
+	{0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF},
+	{0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
+};
+ 
+byte Inv_S_Box[16][16] = {
+	{0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},
+	{0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},
+	{0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},
+	{0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},
+	{0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},
+	{0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},
+	{0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},
+	{0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},
+	{0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},
+	{0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},
+	{0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},
+	{0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},
+	{0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},
+	{0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},
+	{0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},
+	{0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}
+};
+ 
+// 轮常数，密钥扩展中用到。（AES-128只需要10轮）
+word Rcon[10] = {0x01000000, 0x02000000, 0x04000000, 0x08000000, 0x10000000, 
+				 0x20000000, 0x40000000, 0x80000000, 0x1b000000, 0x36000000};
+ 
+/**********************************************************************/  
+/*                                                                    */  
+/*                              AES算法实现                           */  
+/*                                                                    */  
+/**********************************************************************/ 
+ 
+/******************************下面是加密的变换函数**********************/
+/**
+ *  S盒变换 - 前4位为行号，后4位为列号
+ */
+void SubBytes(byte mtx[4*4])
 {
-    int c = 0;
-    if(a<b) swap(a,b);
-    c = b;
-    do
-    {
-        b = c;
-        c = a%b;
-        a = b;
-    }
-    while (c != 0);
-    return b;
+	for(int i=0; i<16; ++i)
+	{
+		int row = mtx[i][7]*8 + mtx[i][6]*4 + mtx[i][5]*2 + mtx[i][4];
+		int col = mtx[i][3]*8 + mtx[i][2]*4 + mtx[i][1]*2 + mtx[i][0];
+		mtx[i] = S_Box[row][col];
+	}
 }
-
-int PrimarityTest(int a, int i) //判断i是否是素数
+ 
+/**
+ *  行变换 - 按字节循环移位
+ */
+void ShiftRows(byte mtx[4*4])
 {
-    int flag=0;
-    for(a;a<i;a++)
-    {
-        if(i%a==0)
-        {
-            flag=1;
-            break;
-        }
-    }
-    if(flag) return 0;
-    return 1;
-    // complete this part
+	// 第二行循环左移一位
+	byte temp = mtx[4];
+	for(int i=0; i<3; ++i)
+		mtx[i+4] = mtx[i+5];
+	mtx[7] = temp;
+	// 第三行循环左移两位
+	for(int i=0; i<2; ++i)
+	{
+		temp = mtx[i+8];
+		mtx[i+8] = mtx[i+10];
+		mtx[i+10] = temp;
+	}
+	// 第四行循环左移三位
+	temp = mtx[15];
+	for(int i=3; i>0; --i)
+		mtx[i+12] = mtx[i+11];
+	mtx[12] = temp;
 }
-
-int ModularExponention(int a, int b, int n)  //求a^bmodn
-{
-    int y;
-
-    /*使用二进制平方乘法计算 pow(a,b) % n*/
-    y=1;
-
-    while(b != 0)
-    {
-        /*对于b中的每个1，累加y*/
-
-        if(b & 1)
-            y = (y*a) % n;
-
-        /*对于b中的每一位，计算a的平方*/
-        a = (a*a) % n;
-
-        /*准备b中的下一位*/
-        b = b>>1;
-    }
-
-    return y;
-    // complete this part
+ 
+/**
+ *  有限域上的乘法 GF(2^8)
+ */
+byte GFMul(byte a, byte b) { 
+	byte p = 0;
+	byte hi_bit_set;
+	for (int counter = 0; counter < 8; counter++) {
+		if ((b & byte(1)) != 0) {
+			p ^= a;
+		}
+		hi_bit_set = (byte) (a & byte(0x80));
+		a <<= 1;
+		if (hi_bit_set != 0) {
+			a ^= 0x1b; /* x^8 + x^4 + x^3 + x + 1 */
+		}
+		b >>= 1;
+	}
+	return p;
 }
-
-void extgcd(ll a,ll b,ll& d,ll& x,ll& y) //获取(1/a)modb得结果
+ 
+/**
+ *  列变换
+ */
+void MixColumns(byte mtx[4*4])
 {
-    if(!b)
-    {
-        d=a;
-        x=1;
-        y=0;
-    }
-    else
-    {
-        extgcd(b,a%b,d,y,x);
-        y-=x*(a/b);
-    }
+	byte arr[4];
+	for(int i=0; i<4; ++i)
+	{
+		for(int j=0; j<4; ++j)
+			arr[j] = mtx[i+j*4];
+ 
+		mtx[i] = GFMul(0x02, arr[0]) ^ GFMul(0x03, arr[1]) ^ arr[2] ^ arr[3];
+		mtx[i+4] = arr[0] ^ GFMul(0x02, arr[1]) ^ GFMul(0x03, arr[2]) ^ arr[3];
+		mtx[i+8] = arr[0] ^ arr[1] ^ GFMul(0x02, arr[2]) ^ GFMul(0x03, arr[3]);
+		mtx[i+12] = GFMul(0x03, arr[0]) ^ arr[1] ^ arr[2] ^ GFMul(0x02, arr[3]);
+	}
 }
-
-int ModularInverse(int a,int b)  //获取(1/a)modb得结果
+ 
+/**
+ *  轮密钥加变换 - 将每一列与扩展密钥进行异或
+ */
+void AddRoundKey(byte mtx[4*4], word k[4])
 {
-    ll d,x,y;
-    extgcd(a,b,d,x,y);
-    return d==1?(x+b)%b:-1;
-    // complete this part
+	for(int i=0; i<4; ++i)
+	{
+		word k1 = k[i] >> 24;
+		word k2 = (k[i] << 8) >> 24;
+		word k3 = (k[i] << 16) >> 24;
+		word k4 = (k[i] << 24) >> 24;
+		
+		mtx[i] = mtx[i] ^ byte(k1.to_ulong());
+		mtx[i+4] = mtx[i+4] ^ byte(k2.to_ulong());
+		mtx[i+8] = mtx[i+8] ^ byte(k3.to_ulong());
+		mtx[i+12] = mtx[i+12] ^ byte(k4.to_ulong());
+	}
 }
-
-void KeyGeneration()  //获取公钥密钥
+ 
+/**************************下面是解密的逆变换函数***********************/
+/**
+ *  逆S盒变换
+ */
+void InvSubBytes(byte mtx[4*4])
 {
-    int p, q;
-    int phi_n;
-
-    do
-    {
-        do
-            p = rand();
-        while (p % 2 == 0);
-
-    }
-    while (!PrimarityTest(2, p));
-
-    do
-    {
-        do
-            q = rand();
-        while (q % 2 == 0);
-    }
-    while (!PrimarityTest(2, q));
-
-    n = p * q;
-    phi_n = (p - 1) * (q - 1);
-
-    do
-        e = rand() % (phi_n - 2) + 2; // 1 < e < phi_n
-    while (gcd(e, phi_n) != 1);
-
-    d = ModularInverse(e,phi_n);
+	for(int i=0; i<16; ++i)
+	{
+		int row = mtx[i][7]*8 + mtx[i][6]*4 + mtx[i][5]*2 + mtx[i][4];
+		int col = mtx[i][3]*8 + mtx[i][2]*4 + mtx[i][1]*2 + mtx[i][0];
+		mtx[i] = Inv_S_Box[row][col];
+	}
 }
-
-void Encryption(int value, FILE* out)  //加密
+ 
+/**
+ *  逆行变换 - 以字节为单位循环右移
+ */
+void InvShiftRows(byte mtx[4*4])
 {
-    int cipher;
-    cipher = ModularExponention(value, e, n);
-    fprintf(out, "%d ", cipher);
+	// 第二行循环右移一位
+	byte temp = mtx[7];
+	for(int i=3; i>0; --i)
+		mtx[i+4] = mtx[i+3];
+	mtx[4] = temp;
+	// 第三行循环右移两位
+	for(int i=0; i<2; ++i)
+	{
+		temp = mtx[i+8];
+		mtx[i+8] = mtx[i+10];
+		mtx[i+10] = temp;
+	}
+	// 第四行循环右移三位
+	temp = mtx[12];
+	for(int i=0; i<3; ++i)
+		mtx[i+12] = mtx[i+13];
+	mtx[15] = temp;
 }
-
-void Decryption(int value, FILE* out)  //解密
+ 
+void InvMixColumns(byte mtx[4*4])
 {
-    int decipher;
-    decipher = ModularExponention(value, d, n);
-    fprintf(out, "%c", decipher);
+	byte arr[4];
+	for(int i=0; i<4; ++i)
+	{
+		for(int j=0; j<4; ++j)
+			arr[j] = mtx[i+j*4];
+ 
+		mtx[i] = GFMul(0x0e, arr[0]) ^ GFMul(0x0b, arr[1]) ^ GFMul(0x0d, arr[2]) ^ GFMul(0x09, arr[3]);
+		mtx[i+4] = GFMul(0x09, arr[0]) ^ GFMul(0x0e, arr[1]) ^ GFMul(0x0b, arr[2]) ^ GFMul(0x0d, arr[3]);
+		mtx[i+8] = GFMul(0x0d, arr[0]) ^ GFMul(0x09, arr[1]) ^ GFMul(0x0e, arr[2]) ^ GFMul(0x0b, arr[3]);
+		mtx[i+12] = GFMul(0x0b, arr[0]) ^ GFMul(0x0d, arr[1]) ^ GFMul(0x09, arr[2]) ^ GFMul(0x0e, arr[3]);
+	}
 }
-int main(void)
+ 
+/******************************下面是密钥扩展部分***********************/
+/**
+ * 将4个 byte 转换为一个 word.
+ */
+word Word(byte& k1, byte& k2, byte& k3, byte& k4)
 {
-    FILE* inp, * out;
-    char filepath[15], filename[100];
-
-    strcpy(filepath, "F:\Desktop\\");  //文件路径
-
-    sprintf(filename, "%s%s", filepath, "cipher.txt");
-    out = fopen(filename, "w+");  //打开文件
-    fclose(out);
-    sprintf(filename, "%s%s", filepath, "decipher.txt");
-    out = fopen(filename, "w+");  //打开文件
-    fclose(out);
-
-    KeyGeneration();  //获取公钥密钥
-
-    sprintf(filename, "%s%s", filepath, "plain.txt");
-    inp = fopen(filename, "r+");  //读取原文件
-    if (inp == NULL)
-    {
-        printf("Error opening Source File.\n");
-        exit(1);
-    }
-
-    sprintf(filename, "%s%s", filepath, "cipher.txt");
-    out = fopen(filename, "w+");
-    if (out == NULL)
-    {
-        printf("Error opening Destination File.\n");
-        exit(1);
-    }
-
-    // encryption starts
-    while (1)
-    {
-        char ch = getc(inp);  //读取文件字符，一个字符一个字符的读取输出
-        if (ch == -1)
-            break;
-        int value = toascii(ch);  //toascii将字符转化成对应ascall值
-        Encryption(value, out);  //加密输出
-    }
-
-    fclose(inp);
-    fclose(out);
-
-    // decryption starts
-    sprintf(filename, "%s%s", filepath, "cipher.txt");
-    inp = fopen(filename, "r");
-    if (inp == NULL)
-    {
-        printf("Error opening Cipher Text.\n");
-        exit(1);
-    }
-
-    sprintf(filename, "%s%s", filepath, "decipher.txt");
-    out = fopen(filename, "w+");
-    if (out == NULL)
-    {
-        printf("Error opening File.\n");
-        exit(1);
-    }
-
-    while (1)
-    {
-        int cip;
-        if (fscanf(inp, "%d", &cip) == -1)
-            break;
-        Decryption(cip, out);  //解密
-    }
-    fclose(out);
-
-    return 0;
+	word result(0x00000000);
+	word temp;
+	temp = k1.to_ulong();  // K1
+	temp <<= 24;
+	result |= temp;
+	temp = k2.to_ulong();  // K2
+	temp <<= 16;
+	result |= temp;
+	temp = k3.to_ulong();  // K3
+	temp <<= 8;
+	result |= temp;
+	temp = k4.to_ulong();  // K4
+	result |= temp;
+	return result;
+}
+ 
+/**
+ *  按字节 循环左移一位
+ *  即把[a0, a1, a2, a3]变成[a1, a2, a3, a0]
+ */
+word RotWord(word& rw)
+{
+	word high = rw << 8;
+	word low = rw >> 24;
+	return high | low;
+}
+ 
+/**
+ *  对输入word中的每一个字节进行S-盒变换
+ */
+word SubWord(word& sw)
+{
+	word temp;
+	for(int i=0; i<32; i+=8)
+	{
+		int row = sw[i+7]*8 + sw[i+6]*4 + sw[i+5]*2 + sw[i+4];
+		int col = sw[i+3]*8 + sw[i+2]*4 + sw[i+1]*2 + sw[i];
+		byte val = S_Box[row][col];
+		for(int j=0; j<8; ++j)
+			temp[i+j] = val[j];
+	}
+	return temp;
+}
+ 
+/**
+ *  密钥扩展函数 - 对128位密钥进行扩展得到 w[4*(Nr+1)]
+ */ 
+void KeyExpansion(byte key[4*Nk], word w[4*(Nr+1)])
+{
+	word temp;
+	int i = 0;
+	// w[]的前4个就是输入的key
+	while(i < Nk) 
+	{
+		w[i] = Word(key[4*i], key[4*i+1], key[4*i+2], key[4*i+3]);
+		++i;
+	}
+ 
+	i = Nk;
+ 
+	while(i < 4*(Nr+1))
+	{
+		temp = w[i-1]; // 记录前一个word
+		if(i % Nk == 0)
+			w[i] = w[i-Nk] ^ SubWord(RotWord(temp)) ^ Rcon[i/Nk-1];
+		else 
+			w[i] = w[i-Nk] ^ temp;
+		++i;
+	}
+}
+ 
+/******************************下面是加密和解密函数**************************/
+/**
+ *  加密
+ */
+void encrypt(byte in[4*4], word w[4*(Nr+1)])
+{
+	word key[4];
+	for(int i=0; i<4; ++i)
+		key[i] = w[i];
+	AddRoundKey(in, key);
+ 
+	for(int round=1; round<Nr; ++round)
+	{
+		SubBytes(in);
+		ShiftRows(in);
+		MixColumns(in);
+		for(int i=0; i<4; ++i)
+			key[i] = w[4*round+i];
+		AddRoundKey(in, key);
+	}
+ 
+	SubBytes(in);
+	ShiftRows(in);
+	for(int i=0; i<4; ++i)
+		key[i] = w[4*Nr+i];
+	AddRoundKey(in, key);
+}
+ 
+/**
+ *  解密
+ */
+void decrypt(byte in[4*4], word w[4*(Nr+1)])
+{
+	word key[4];
+	for(int i=0; i<4; ++i)
+		key[i] = w[4*Nr+i];
+	AddRoundKey(in, key);
+ 
+	for(int round=Nr-1; round>0; --round)
+	{
+		InvShiftRows(in);
+		InvSubBytes(in);
+		for(int i=0; i<4; ++i)
+			key[i] = w[4*round+i];
+		AddRoundKey(in, key);
+		InvMixColumns(in);
+	}
+ 
+	InvShiftRows(in);
+	InvSubBytes(in);
+	for(int i=0; i<4; ++i)
+		key[i] = w[i];
+	AddRoundKey(in, key);
+}
+ 
+/**********************************************************************/  
+/*                                                                    */  
+/*                              测试                                  */  
+/*                                                                    */  
+/**********************************************************************/ 
+int main()
+{
+	byte key[16] = {0x2b, 0x7e, 0x15, 0x16, 
+					0x28, 0xae, 0xd2, 0xa6, 
+					0xab, 0xf7, 0x15, 0x88, 
+					0x09, 0xcf, 0x4f, 0x3c};
+ 
+	byte plain[16] = {0x32, 0x88, 0x31, 0xe0, 
+					0x43, 0x5a, 0x31, 0x37,
+					0xf6, 0x30, 0x98, 0x07,
+					0xa8, 0x8d, 0xa2, 0x34}; 
+	// 输出密钥
+	cout << "密钥是：";
+	for(int i=0; i<16; ++i)
+		cout << hex << key[i].to_ulong() << " ";
+	cout << endl;
+ 
+	word w[4*(Nr+1)];
+	KeyExpansion(key, w);
+ 
+	// 输出待加密的明文
+	cout << endl << "待加密的明文："<<endl;
+	for(int i=0; i<16; ++i)
+	{
+		cout << hex << plain[i].to_ulong() << " ";
+		if((i+1)%4 == 0)
+			cout << endl;
+	}
+	cout << endl;
+ 
+	// 加密，输出密文
+	encrypt(plain, w);
+	cout << "加密后的密文："<<endl;
+	for(int i=0; i<16; ++i)
+	{
+		cout << hex << plain[i].to_ulong() << " ";
+		if((i+1)%4 == 0)
+			cout << endl;
+	}
+	cout << endl;
+ 
+	// 解密，输出明文
+	decrypt(plain, w);
+	cout << "解密后的明文："<<endl;
+	for(int i=0; i<16; ++i)
+	{
+		cout << hex << plain[i].to_ulong() << " ";
+		if((i+1)%4 == 0)
+			cout << endl;
+	}
+	cout << endl;
+	return 0;
 }
